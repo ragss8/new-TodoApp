446f5a99a0ea35ae90e74686ca195897de5577de
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-05-30 16:19:37 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+// functionalities to be added are: editing a task , render the task and many more.
 
   // Get references
   const taskInput = document.getElementById('taskInput');
@@ -44,24 +45,49 @@
     updateTaskDisplay();
   }
 
-  // Function to update the task display
-  function updateTaskDisplay() {
-    // Clear the current task display
-    taskDisplay.innerHTML = '';
+    // Function to update the task display
+    function updateTaskDisplay() {
+        // Clear the current task display
+        taskDisplay.innerHTML = '';
+    
+        // Loop through the task list and create the task items
+        for (const task of taskList) {
+          // Create a new list item for the task
+          const listItem = document.createElement('li');
+    
+          // Set the task item text
+          listItem.textContent = `Task ID: ${task.id}, Status: ${task.status}, Title: ${task.title}`;
+    
+          //create delButton for the task
+          const delButton = document.createElement('button');
+          delButton.textContent = 'Delete';
+          delButton.addEventListener('click' , () => deleteTask(task.id)); // call function for delButton
+    
+          // Append the delButton to taskList
+          listItem.appendChild(delButton);
+    
+          // Append the task item to the task display
+          taskDisplay.appendChild(listItem);
+        }
+      }
 
-    // Loop through the task list and create the task items
-    for (const task of taskList) {
-      // Create a new list item for the task
-      const listItem = document.createElement('li');
-
-      // Set the task item text
-      listItem.textContent = `Task ID: ${task.id}, Status: ${task.status}, Title: ${task.title}`;
-
-      // Append the task item to the task display
-      taskDisplay.appendChild(listItem);
+      // Function to delete a task from the task list
+function deleteTask(taskId) {
+    // Find the index of the task with the given ID
+    const taskIndex = taskList.findIndex(task => task.id === taskId);
+  
+    // Check if the task exists
+    if (taskIndex === -1) {
+      console.log('Task not found');
+      return;
     }
+  
+    // Remove the task from the taskList array
+    taskList.splice(taskIndex, 1);
+  
+    // Update the task display
+    updateTaskDisplay();
   }
-
   // Function to handle keydown event
   function handleKeyDown(event) {
     if (event.key === 'Enter') {
